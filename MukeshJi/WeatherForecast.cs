ab2094241e0d5f9f9de75949754672a3f5e96655
vijendraparmar12 <135025114+vijendraparmar12@users.noreply.github.com>
2023-06-05 12:08:20 +0530

    first vijendra22 commit

--- MukeshJi/WeatherForecast.cs
+++ MukeshJi/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace MukeshJi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string? Summary2 { get; set; }
     }
 }
